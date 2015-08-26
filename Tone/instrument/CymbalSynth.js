2b0633a8a13790cd3e807a1d2f604aa3bdc64972
Seth Kranzler <sethkranzler@gmail.com>
2015-08-25 20:59:10 -0400

    working on replacing SimpleFM oscillator, not complete

--- Tone/instrument/CymbalSynth.js
+++ Tone/instrument/CymbalSynth.js
@@ -1,11 +1,28 @@
 
 define(["Tone/core/Tone", "Tone/source/Oscillator", "Tone/instrument/Instrument", 
-	"Tone/component/AmplitudeEnvelope", "Tone/component/Filter", 
-	"Tone/signal/Signal", "Tone/signal/Multiply", "Tone/signal/Scale",
-	"Tone/source/PWMOscillator", "Tone/instrument/SimpleFM"],
+	"Tone/component/AmplitudeEnvelope", "Tone/component/Filter", "Tone/source/OmniOscillator",
+	"Tone/signal/Signal", "Tone/signal/Multiply", "Tone/signal/Scale", "Tone/instrument/SimpleFM"],
 function(Tone){
 
 	"use strict";
+
+	/**
+	 *  Inharmonic ratio of frequencies based on the Roland TR-808
+	 *  Taken from https://ccrma.stanford.edu/papers/tr-808-cymbal-physically-informed-circuit-bendable-digital-model
+	 *  @private
+	 *  @static
+	 *  @type {Array}
+	 */
+	var inharmRatios = [1.0, 1.483, 1.932, 2.546, 2.630, 3.897];
+
+	/**
+	 *  Harmonic ratio that produces a more pitched cymbal
+	 *  @private
+	 *  @static
+	 *  @type {Array}
+	 */
+	var harmRatios = [1.0, 1.5, 2.025, 2.975, 4.0, 6.0];
+
 	/**
 	 *  @class  CymbalSynth is composed of 6 Tone.FMSynths, with their 
 	 *  carrier set to a square wave, and the modulator set to a pulse 
@@ -18,7 +35,7 @@ function(Tone){
 	 *                             see defaults below
 	 *  @example
 	 * var cymbal = new Tone.CymbalSynth().toMaster();
-	 * cymbal.harmonicity.value = 10;
+	 * cymbal.inharmonicity.value = 10;
 	 * cymbal.triggerAttack();
 	 */
 	Tone.CymbalSynth = function(options){
@@ -30,74 +47,39 @@ function(Tone){
 		 * The initial strike component of the cymbal hit.
 		 * @type {CymbalComponent}
 		 */
-		this.strike = new CymbalComponent({
-			"volume" : options.strike.volume,
-			"cutoff" : options.strike.cutoff,
-			"frequencyScalar" : 1.886,
-			"resonance" : options.strike.resonance,
-			"resonanceScalar" : 0.533,
-			"envelope" : {
-				"attack": options.strike.envelope.attack, 
-				"decay": options.strike.envelope.decay, 
-			}
-		}).connect(this.output);
+		this.strike = new CymbalComponent(options.strike, 1.886, 0.533).connect(this.output);
 
 		/**
 		 * The body portion of the cymbal hit.
 		 * @type {CymbalComponent}
 		 */
-		this.body = new CymbalComponent({
-			"volume" : options.body.volume,
-			"cutoff" : options.body.cutoff,
-			"frequencyScalar" : 1.643,
-			"resonance" : options.body.resonance,
-			"resonanceScalar" : 0.333,
-			"envelope" : {
-				"attack": options.body.envelope.attack, 
-				"decay": options.body.envelope.decay, 
-			}
-		}).connect(this.output);
+		this.body = new CymbalComponent(options.body, 1.643, 0.333).connect(this.output);
 
 		/**
-		 *  [harmonicity description]
+		 *  The level of inharmonicity between the frequencies of the oscillators. 
+		 *  Interesting sounds can be had between 0 and 20.
 		 *  @type {Positive}
 		 *  @signal
 		 */
-		this.harmonicity = new Tone.Signal(options.harmonicity);
-		this.harmonicity.units = Tone.Type.Positive;
+		this.inharmonicity = new Tone.Signal(options.inharmonicity, Tone.Type.Positive);
 
 		/**
 		 *  The base frequency for the series of oscillators. 
 		 *  All oscillators scale in a harmonic or inharmonic pattern
-		 *  depending on the value of the harmonicity.
+		 *  depending on the value of the inharmonicity.
 		 *  @type {Frequency}
 		 *  @signal
 		 */
 		this.frequency = new Tone.Signal(options.frequency, Tone.Type.Frequency);
 
 		/**
-		 *  Inharmonic ratio of frequencies based on the Roland TR-808
-		 *  Taken from https://ccrma.stanford.edu/papers/tr-808-cymbal-physically-informed-circuit-bendable-digital-model
-		 *  @private
-		 *  @static
-		 *  @type {Array}
-		 */
-		this.inharmRatios = [1.0, 1.483, 1.932, 2.546, 2.630, 3.897];
-
-		/**
-		 *  Harmonic ratio that produces a more pitched cymbal
-		 *  @private
-		 *  @static
-		 *  @type {Array}
-		 */
-		this.harmRatios = [1.0, 1.5, 2.025, 2.975, 4.0, 6.0];
-
-		/**
 		 *  A series of FMSynths
 		 *  @private
 		 *  @type {Array}
 		 */
-		this._oscillators = [];
+		this._carriers = [];
+
+		this._modulators = [];
 
 		/**
 		 *  Tone.ScaledSignals used for interpolating between harmonic
@@ -114,32 +96,65 @@ function(Tone){
 		 */
 		this._freqMult  = [];
 
+		this.harmonicity = [];
+
+		this._modulationNodes = [];
+
+		this._modulationIndex = [];
+
 		//make all the FMSynths, connect the frequency signals
 		for(var i = 0; i < 6; i++){
-			this._scaledSignals[i] = new Tone.Scale(this.inharmRatios[i], this.harmRatios[i]);
+			this._scaledSignals[i] = new Tone.Scale(inharmRatios[i], harmRatios[i]);
 			this._freqMult[i] = new Tone.Multiply();
-			this._oscillators[i] = new Tone.SimpleFM({
-				"harmonicity" : 1.5,
-				"modulationIndex" : 10,
-				"carrier" : {
-					"oscillator" : {
-						"type" : "square"
-					}
-				},
-				"modulator" : {
-					"oscillator" : {
-						"type" : "pulse",
-						"width" : 0.75
-					}
-				},
-			});
-			this.harmonicity.connect(this._scaledSignals[i]);
+			this._carriers[i] = new Tone.Oscillator();
+			this._carriers[i].type = "square";
+			this._carriers[i].volume.value = -10;
+			
+			this._modulators[i] = new Tone.OmniOscillator();
+			this._modulators[i].type = "pulse";
+			this._modulators[i].width.value = 0.75;
+			this._modulators[i].volume.value = -10;
+
+			this.harmonicity[i] = new Tone.Multiply(1.5);
+
+			this._modulationIndex[i] = new Tone.Multiply(10);
+
+
+			this.inharmonicity.connect(this._scaledSignals[i]);
 			this._scaledSignals[i].connect(this._freqMult[i], 0, 0);
 			this.frequency.connect(this._freqMult[i], 0, 1);
-			this._freqMult[i].connect(this._oscillators[i].frequency);
-			this._oscillators[i].connect(this.strike);
-			this._oscillators[i].connect(this.body);
-			this._oscillators[i].triggerAttack(Tone.context.currentTime);
+
+			this._modulationNodes[i] = this.context.createGain();
+			this._modulators[i].connect(this._modulationNodes[i].gain);
+
+			this._freqMult[i].chain(this.harmonicity[i], this._modulationNodes[i]);
+			this._freqMult[i].chain(this._modulationIndex[i], this._modulationNodes[i]);
+			this._modulationNodes[i].gain.value = 0;
+
+			this._modulationNodes[i].connect(this._carriers[i].frequency);
+			this._freqMult[i].connect(this._carriers[i].frequency);
+			this._carriers[i].connect(this.strike);
+			this._carriers[i].connect(this.body);
+
+			this._carriers[i].start();
+			this._modulators[i].start();
+
+
+			// this._oscillators[i] = new Tone.SimpleFM({
+			// 	"harmonicity" : 1.5,
+			// 	"modulationIndex" : 10,
+			// 	"carrier" : {
+			// 		"oscillator" : {
+			// 			"type" : "square"
+			// 		}
+			// 	},
+			// 	"modulator" : {
+			// 		"oscillator" : {
+			// 			"type" : "pulse",
+			// 			"width" : 0.75
+			// 		}
+			// 	},
+			// });
 		}
 	};
 
@@ -152,7 +167,7 @@ function(Tone){
 	 *  @type {Object}
 	 */
 	Tone.CymbalSynth.defaults = {
-		"harmonicity" : 0,
+		"inharmonicity" : 0,
 		"frequency" : 200,
 		"strike" : {
 			"volume" : 0,
@@ -160,7 +175,8 @@ function(Tone){
 			"resonance" : 6,
 			"envelope": { 
 				"attack" : 0.01, 
-				"decay" : 0.25
+				"decay" : 0.25,
+				"release" : 0
 			}
 		},
 		"body" : {
@@ -169,7 +185,8 @@ function(Tone){
 			"resonance" : 6,
 			"envelope" : {
 				"attack" : 0.01,
-				"decay" : 1.5
+				"decay" : 1.5,
+				"release": 0.3
 			}
 
 
@@ -215,14 +232,14 @@ function(Tone){
 	 */
 	Tone.CymbalSynth.prototype.dispose = function(){
 		Tone.Instrument.prototype.dispose.call(this);
-		this.harmonicity.dispose();
-		this.harmonicity = null;
+		this.inharmonicity.dispose();
+		this.inharmonicity = null;
 		this.frequency.dispose();
 		this.frequency = null;
-		this.inharmRatios = null;
-		this.harmRatios = null;
+		//this.inharmRatios = null;
+		//this.harmRatios = null;
 
-		for(var i = 0; i <6; i++){
+		for(var i = 0; i <this._oscillators.length; i++){
 			this._oscillators[i].dispose();
 			this._scaledSignals[i].dispose();
 			this._freqMult[i].dispose();
@@ -239,66 +256,63 @@ function(Tone){
 	};
 
 	/**
-	 * Cymbal part helper class
-	 * @private
+	 *  CymbalComponent helper class
+	 *  @param {Object} options         the options for the cymbal component
+	 *  @param {number} frequencyScalar the ratio between the bandpass filter (input)
+	 *                                  and the highpass filter (output)
+	 *  @param {number} resonanceScalar the ratio between bandpass and higpass resonance
 	 */
-	var CymbalComponent = function(options){
+	var CymbalComponent = function(options, frequencyScalar, resonanceScalar){
 		this.output = this.context.createGain();
 		this.volume = new Tone.Signal({
 			"param" : this.output.gain, 
 			"units" : Tone.Type.Decibels,
 			"value" : options.volume
 		});
-		this._readOnly(["volume"]);
-		this._highPass = new Tone.Filter({
+		this._filter = new Tone.Filter({
 			"type" : "highpass"
 		}).connect(this.output);
 		this.envelope = new Tone.AmplitudeEnvelope({
 			"attack" : options.envelope.attack,
 			"decay" : options.envelope.decay,
 			"sustain" : 0,
-			"release": 0,
+			"release": options.envelope.release,
 			"attackCurve" : "exponential"
 
-		}).connect(this._highPass);
+		}).connect(this._filter);
 		this.input = new Tone.Filter({
 			"type" : "bandpass",
-			"Q" : options.Q
 		}).connect(this.envelope);
-		this.cutoff = new Tone.Signal(options.cutoff).connect(this.input.frequency);
-		this._highPassFrequency = new Tone.Multiply(options.frequencyScalar).connect(this._highPass.frequency);
-		this.cutoff.connect(this._highPassFrequency);
-		this.resonance = new Tone.Signal(options.resonance).connect(this.input.Q);
-		this._highPassResonance = new Tone.Multiply(options.resonanceScalar).connect(this._highPass.Q);
-		this.resonance.connect(this._highPassResonance);
-	};
-
-	CymbalComponent.prototype.dispose = function(){
-		this.output.dispose();
-		this.output = null;
-		this.volume.dispose();
-		this.volume = null;
-		this._highPass.dispose();
-		this._highPass = null;
-		this.envelope.dispose();
-		this.envelope = null;
-		this.input.dispose();
-		this.input = null;
-		this.cutoff.dispose();
-		this.cutoff = null;
-		this._highPassFrequency.dispose();
-		this._highPassFrequency = null;
-		this.resonance.dispose();
-		this.resonance = null;
-		this._highPassResonance.dispose();
-		this._highPassResonance = null;
-		return this;
+		this.cutoff = this.input.frequency;
+		this.cutoff.value = options.cutoff;
+		this._scaledFrequency = new Tone.Multiply(frequencyScalar).connect(this._filter.frequency);
+		this.cutoff.connect(this._scaledFrequency);
+		this.resonance = this.input.Q;
+		this.resonance.value = options.resonance;
+		this._scaledResonance = new Tone.Multiply(resonanceScalar).connect(this._filter.Q);
+		this.resonance.connect(this._scaledResonance);
 	};
 
 	Tone.extend(CymbalComponent);
 
+	CymbalComponent.prototype.dispose = function(){
+		Tone.prototype.dispose.call(this);
+		this.volume.dispose();
+		this.volume = null;
+		this._filter.dispose();
+		this._filter = null;
+		this.envelope.dispose();
+		this.envelope = null;
+		this.cutoff.dispose();
+		this.cutoff = null;
+		this._scaledFrequency.dispose();
+		this._scaledFrequency = null;
+		this.resonance.dispose();
+		this.resonance = null;
+		this._scaledResonance.dispose();
+		this._scaledResonance = null;
+		return this;
+	};
+
 	return Tone.CymbalSynth;
 });
-
-
-
